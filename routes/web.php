844c1cd499578f04b0046d322d8d19e2d668792a
edwinbnto <edwin.benito@cvsu.edu.ph>
2026-06-27 16:58:10 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/order', function () {
+    return view('order');
+})->name('orders');
+
+Route::get('/packing', function () {
+    return view('packing');
+})->name('packing');
+
+Route::get('/shipping', function () {
+    return view('shipping');
+})->name('shipping');
+
+Route::get('/return', function () {
+    return view('return');
+})->name('return');
