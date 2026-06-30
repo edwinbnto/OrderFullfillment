3f9e3baadd39a3c879b6b57f429f3d629bb0a075
edwinbnto <edwin.benito@cvsu.edu.ph>
2026-06-30 13:27:05 +0800

    describe what you changed here

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('Signup');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/order', function () {
+    return view('order');
+})->name('orders');
+
+Route::get('/packing', function () {
+    return view('packing');
+})->name('packing');
+
+Route::get('/shipping', function () {
+    return view('shipping');
+})->name('shipping');
+
+Route::get('/return', function () {
+    return view('return');
+})->name('return');
+
+Route::get('/orderfloat', function () {
+    return view('orderfloat');
+})->name('orderfloat');
+
+Route::get('/database', function () {
+    return view('database');
+})->name('database');
+
+Route::get('/contactus', function () {
+    return view('contactus');
+})->name('contactus');
