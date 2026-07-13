14ac2a4b6d1d4f367b741d5b332adc2aaa37db76
edwinbnto <edwin.benito@cvsu.edu.ph>
2026-07-13 18:10:14 +0800

    pack and pdsql

---
+++ database/migrations/2026_07_13_100647_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->string('id')->primary(); // e.g. #ORD-5123
+            $table->string('customer_name');
+            $table->string('product_name');
+            $table->integer('qty');
+            $table->string('status')->default('NEW'); // NEW, PACKING, etc.
+            $table->date('due_date');
+            $table->timestamps(); // created_at, updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
