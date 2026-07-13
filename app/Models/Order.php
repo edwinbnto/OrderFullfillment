088d352ff4984c5b0992aa2d55c88e1764988071
edwinbnto <edwin.benito@cvsu.edu.ph>
2026-07-14 07:23:31 +0800

    add auto increment id

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = 'id';
-public $incrementing = false;
-protected $keyType = 'string';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $guarded = ['id'];
 }
-
-
